769771e3190708cc5f119dd6b7b36106da71c466
Federico Simonetta <federico.simonetta@unimi.it>
2019-08-13 17:31:01 +0200

    Superfast song retrieval
    Former-commit-id: 1903a3a2a1a1ac2cb74f5c86ea2f4d44f23c7737
    Former-commit-id: ad361402ef3fec8beb16bcd4cf86eb796ea006aa

--- test_utils.pyx
+++ test_utils.pyx
@@ -1,39 +1,40 @@
+# cython: language_level=3
 import json
 from fuzzywuzzy import fuzz
 import sqlite3 as sql
 import numpy as np
+import psutil
 from libcpp.string cimport string
+from libcpp.vector cimport vector
 from libcpp cimport bool
 import multiprocessing as mp
 import cython
 from cython.parallel import prange
 
-db = sql.connect('./kunstderfuge.db')
-count = mp.Value('i', 0)
 lock = mp.Lock()
 
 cdef extern from "<cctype>" namespace "std":
-    int tolower( int ch )
-    int isdigit( int ch )
+    int tolower( int ch ) nogil
+    int isdigit( int ch ) nogil
 
-cdef string split_digits_from_words(string cppstring):
-    cdef int i = 0
+cdef string split_digits_from_words(string cppstring) nogil:
+    cdef size_t i = 0
     while cppstring.length() > i + 1:
-        if isdigit(cppstring[i+1]) and not isdigit(cppstring[i]) and cppstring[i] != ' ':
-            cppstring.insert(i+1, ' ')
-        if not isdigit(cppstring[i+1]) and isdigit(cppstring[i]) and cppstring[i+1] != ' ':
-            cppstring.insert(i+1, ' ')
+        if isdigit(cppstring[i+1]) and not isdigit(cppstring[i]) and cppstring[i] != b' ':
+            cppstring.insert(i+1, b' ')
+        if not isdigit(cppstring[i+1]) and isdigit(cppstring[i]) and cppstring[i+1] != b' ':
+            cppstring.insert(i+1, b' ')
         i+=1
     return cppstring
 
-cdef list get_composer_list(string composer):
-    global lock
-    with lock:
-        query = db.execute("SELECT Opus, Title FROM Composers JOIN Songs ON Composers.ID = Songs.Composer WHERE Composers.Surname = '" + composer.decode('UTF-8') + "'")
-    return query.fetchall()
+cdef vector[vector[string]] get_composer_list(string composer):
+    db = sql.connect('./kunstderfuge.db')
+    db.text_factory = bytes
+    query = db.execute("SELECT Opus, Title FROM Composers JOIN Songs ON Composers.ID = Songs.Composer WHERE Composers.Surname = '" + composer.decode('UTF-8') + "'")
+    return <vector[vector[string]]> query.fetchall()
 
-cdef string replace(string cppstring, string a, string b):
-    cdef int i
+cdef string replace(string cppstring, string a, string b) nogil:
+    cdef size_t i
     cdef char c
     for i in range(cppstring.length()):
         c = cppstring[i]
@@ -42,44 +43,52 @@ cdef string replace(string cppstring, string a, string b):
     return cppstring
 
 cdef string norm(string cppstring):
-    cdef int i
+    cdef size_t i
     for i in range(cppstring.length()):
         cppstring[i] = tolower(cppstring[i])
-    cppstring = replace(cppstring, '_', ' ')
-    cppstring = replace(cppstring, '-', ' ')
-    cppstring = replace(cppstring, '.', ' ')
+    cppstring = replace(cppstring, b'_', b' ')
+    cppstring = replace(cppstring, b'-', b' ')
+    cppstring = replace(cppstring, b'.', b' ')
     cppstring = split_digits_from_words(cppstring)
     return cppstring
 
-cdef list search(string title, string composer):
-    title = norm(title)
-    cdef list query = get_composer_list(composer)
-    if len(query) == 0:
-        print('cannot retrieve composer', composer)
-    cdef list answer = []
-    cdef tuple i
+cdef vector[vector[string]] search(string title, string composer):
+    cdef vector[vector[string]] query
+    cdef size_t score
     cdef string fulltitle
-    cdef int score
+    cdef vector[string] i
+    title = norm(title)
+    query = get_composer_list(composer)
+    if query.size() == 0:
+        print('cannot retrieve composer', composer)
+    cdef vector[vector[string]] answer
     for i in query:
-        fulltitle = norm(i[0].encode('UTF-8') + i[1].encode('UTF-8'))
+        fulltitle = norm(i[0] + i[1])
         score = fuzz.token_set_ratio(title, fulltitle)
         if score > 90:
-            answer.append((fulltitle, title, score))
+            answer.push_back(<vector[string]> [fulltitle, title, str(score).encode('UTF-8')])
     return answer
 
 def pool_work(song):
-    global count
-    answer = search(song['title'].encode('UTF-8'), song['composer'].encode('UTF-8'))
-    if answer:
-        print(len(answer), max(answer, key=lambda x: x[2]))
-        count += 1
+    cdef string composer, title
+    title = song['title'].encode('UTF-8')
+    composer = song['composer'].encode('UTF-8')
+    cdef vector[vector[string]] answer
+    answer = search(title, composer)
+    if answer.size() > 0:
+        print(answer.size(), max(answer, key=lambda x: int(x[2])))
+        return 1
+    else:
+        return 0
 
 def full_comparison():
     cdef dict d = json.load(open('datasets.json'))
-    cdef dict dataset, song
-    cdef list answer
+    cdef dict dataset
+    cdef size_t chunksize, cpu_count = psutil.cpu_count() - 1
+    cdef int count = 0
     for dataset in d['datasets']:
-        with mp.Pool() as pool:
-            pool.map(pool_work, dataset['songs'])
-
+        chunksize = int(len(d['datasets']) / cpu_count) + 1
+        with mp.Pool(cpu_count) as pool:
+            counts = pool.imap_unordered(pool_work, dataset['songs'], chunksize=chunksize)
+            count += sum(counts)
     print(count)
