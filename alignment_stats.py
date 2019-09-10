5df6e4a525fc36738f5cd767b621ebe275ff5e7a
Federico Simonetta <federico.simonetta@unimi.it>
2019-09-10 17:20:03 +0200

    Generation of misaligned data working, still errors in PHENICX and similar
    Former-commit-id: 18673cdb19d59c4b70b067a66a1a422ba75fd585
    Former-commit-id: 42d3f3cb301ffc9d06b7cb8479469f9f2e3ffb61

---
+++ alignment_stats.py
@@ -0,0 +1,121 @@
+from audioscoredataset import Dataset
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+import plotly.graph_objects as go
+import plotly.offline as plt
+from random import choices, uniform
+import pickle
+
+
+data = Dataset('datasets.json')
+
+class Stats:
+    def __init__(self):
+        self.ons_diffs = []
+        self.offs_diffs = []
+        self.means = []
+        self.ons_dev = []
+        self.offs_dev = []
+
+    def add_data(self, ons_diffs, offs_diffs):
+        """
+        Method to add data, then you should still compute histograms
+        """
+        self.ons_dev.append(np.std(ons_diffs))
+        self.offs_dev.append(np.std(offs_diffs))
+        self.means.append(np.mean([offs_diffs, ons_diffs]))
+        self.ons_diffs += StandardScaler().fit_transform(ons_diffs.reshape(-1, 1)).tolist()
+        self.offs_diffs += StandardScaler().fit_transform(offs_diffs.reshape(-1, 1)).tolist()
+
+    def compute_hist(self):
+        """
+        Compute all the histograms in tuples (histogram, bin_edges):
+        self.ons_hist
+        self.offs_hist
+        self.means_hist
+        self.ons_dev_hist
+        self.offs_dev_hist
+        """
+        self.ons_hist = np.histogram(self.ons_diffs, bins='auto', density=True)
+        self.offs_hist = np.histogram(self.offs_diffs, bins='auto', density=True)
+        self.means_hist = np.histogram(self.means, bins='auto', density=True)
+        self.ons_dev_hist = np.histogram(self.ons_dev, bins='auto', density=True)
+        self.offs_dev_hist = np.histogram(self.offs_dev, bins='auto', density=True)
+
+    def get_random_onset_dev(self, k=1):
+        return _get_random_value_from_hist(self.ons_dev_hist, k)
+
+    def get_random_offset_dev(self, k=1):
+        return _get_random_value_from_hist(self.offs_dev_hist, k)
+
+    def get_random_mean(self, k=1):
+        return _get_random_value_from_hist(self.means_hist, k)
+
+    def get_random_onset_diff(self, k=1):
+        return _get_random_value_from_hist(self.ons_hist, k)
+
+    def get_random_offset_diff(self, k=1):
+        return _get_random_value_from_hist(self.offs_hist, k)
+
+
+def seed():
+    """
+    Apply a seed to the python random module to reproduce my results
+    """
+    import random
+    random.seed(1992)
+
+def _get_random_value_from_hist(hist, k=1):
+    """
+    Given a histogram (tuple returned by np.histogram), returns a random value
+    picked with uniform distribution from a bin of the histogram. The bin is
+    picked following the histogram distribution.
+    """
+    start = choices(hist[1][:-1], weights=hist[0], k=k)
+    bin_w = abs(hist[1][1] - hist[1][0])
+    end = np.array(start) + bin_w
+    return [uniform(start[i], end[i]) for i in range(len(start))]
+
+def fill_stats(alignments):
+    stats = Stats()
+
+    for alignment in alignments:
+        data.filter(ground_truth=["non_aligned", alignment])
+
+        for i in range(len(data)):
+            mat_aligned = data.get_score(i, score_type=alignment)
+            mat_score = data.get_score(i, score_type='non_aligned')
+            try:
+                ons_diffs = mat_score[:, 1] - mat_aligned[:, 1]
+                offs_diffs = mat_score[:, 2] - mat_aligned[:, 2]
+            except:
+                import ipdb; ipdb.set_trace()
+            stats.add_data(ons_diffs, offs_diffs)
+
+    return stats
+
+if __name__ == '__main__':
+    stats = fill_stats(['precise_alignment'])
+    # _ons_hist, _offs_hist = take_hist('broad_alignment')
+    stats.compute_hist()
+    seed()
+    v1 = stats.get_random_onset_dev()
+    v2 = stats.get_random_offset_dev()
+    v3 = stats.get_random_mean()
+    v4 = stats.get_random_offset_diff()
+    v5 = stats.get_random_onset_diff()
+    print("Testing getting random value")
+    print(v1, v2, v3, v4, v5)
+
+    pickle.dump(stats, open("alignment_stats.pkl", 'wb'))
+
+    fig1 = go.Figure(data=[go.Scatter(y=stats.ons_hist[0], x=stats.ons_hist[1])])
+    plt.plot(fig1, filename='ons.html')
+    fig2 = go.Figure(data=[go.Scatter(y=stats.offs_hist[0], x=stats.offs_hist[1])])
+    plt.plot(fig2, filename='offs.html')
+    fig3 = go.Figure(data=[go.Scatter(y=stats.means_hist[0], x=stats.means_hist[1])])
+    plt.plot(fig3, filename='ons_means.html')
+    fig5 = go.Figure(data=[go.Scatter(y=stats.ons_dev_hist[0], x=stats.ons_dev_hist[1])])
+    plt.plot(fig5, filename='ons_devs.html')
+    fig6 = go.Figure(data=[go.Scatter(y=stats.offs_dev_hist[0], x=stats.offs_dev_hist[1])])
+    plt.plot(fig6, filename='offs_devs.html')
