c60f7e108e172887e2b6a4aafef5464f4b156222
Federico Simonetta <federico.simonetta@unimi.it>
2021-03-16 19:32:59 +0100

    Fixed asap script and added asap group to Maestro.json

--- asmd/asap.py
+++ asmd/asap.py
@@ -9,8 +9,9 @@ import tempfile
 import zipfile
 from pathlib import Path
 from typing import List, Mapping, Set, Tuple
-
-import requests
+from urllib.request import urlretrieve, urlcleanup
+from alive_progress import alive_bar
+import shutil
 
 from .asmd import Dataset
 from .dataset_utils import filter
@@ -21,21 +22,27 @@ ASAP_URL = "https://github.com/fosfrancesco/asap-dataset/archive/v1.1.zip"
 
 def modify_maestro_definifion(index: List[Tuple[Path, Path]]) -> Mapping:
     """
-    This function was run only once to add the proper group `asap` on the
+    This function was run only once to add the proper group `asap` to the
     `Maestro` dataset
     """
     # create a daset for loading the Maestro definition
-    for definition in Dataset().datasets:
+    dataset = Dataset()
+    for definition in dataset.datasets:
         if definition['name'] == 'Maestro':
             break
 
     # convert index to Set of string for faster search (the `in` operation)
-    _index: Set[str] = {str(e[0]) for e in index}
+    _index: List[str] = [str(e[0]) for e in index]
 
+    install_dir = Path(dataset.install_dir)
     # add `asap` to each song with ground_truth in the index
     for song in definition['songs']:
-        if song['ground_truth'][0] in _index:
-            song.groups.append("asap")
+        path = str((install_dir / song['recording']['path'][0]).with_suffix(''))
+        if path in _index:
+            song['groups'].append("asap")
+        del song['included']
+
+    del definition['included']
     return definition
 
 
@@ -45,15 +52,16 @@ def download_asap() -> tempfile.TemporaryDirectory:
     """
     # downloading
     print("Downloading ASAP")
-    res = requests.get(ASAP_URL)
 
     asap_dir = tempfile.TemporaryDirectory()
-    with tempfile.TemporaryFile() as f:
-        f.write(res.content)
-        print("Uncompressing ASAP")
-        with zipfile.ZipFile(f, 'r') as zip_ref:
-            zip_ref.extractall(str(asap_dir))
+    with alive_bar(unknown='notes2', spinner='notes_scrolling') as bar:
+        temp_fn, _header = urlretrieve(ASAP_URL,
+                                       reporthook=lambda x, y, z: bar)
+    print("Uncompressing ASAP")
+    with zipfile.ZipFile(temp_fn, 'r') as zip_ref:
+        zip_ref.extractall(str(asap_dir))
 
+    urlcleanup()
     return asap_dir
 
 
@@ -68,19 +76,22 @@ def make_index(asap_path: Path) -> List[Tuple[Path, Path]]:
                                       datasets=['Maestro']).get_gts_paths(0)[0]
     # the second occurrence of `/` in the random path
     _idx = asmd_maestro_random_path.index('/',
-                                          asmd_maestro_random_path.index('/'))
+                                          asmd_maestro_random_path.index('/') + 1)
 
     # construct path to asmd Maestro
     asmd_maestro = Path(dataset.install_dir) / asmd_maestro_random_path[:_idx]
 
     out: List[Tuple[Path, Path]] = []
-    with open(asap_path / 'metadata.csv') as f:
-        for row in csv.DictReader(f):
-            maestro_path = row['maestro_midi_performance']
-            if maestro_path:
-                out.append(
-                    (Path(maestro_path.replace('{maestro}', asmd_maestro)),
-                     asap_path / row['midi_score']))
+    # this glob allows to abstracting over directory structure and names
+    for fname in asap_path.glob('**/metadata.csv'):
+        with open(fname) as f:
+            for row in csv.DictReader(f):
+                maestro_path = row['maestro_midi_performance']
+                if maestro_path:
+                    out.append(
+                        (Path(maestro_path.replace('{maestro}',
+                                                   str(asmd_maestro))).with_suffix(''),
+                         asap_path / row['midi_score']))
 
     return out
 
@@ -101,11 +112,14 @@ if __name__ == '__main__':
     argparser.add_argument("-m", "--modify", action='store_true')
     args = argparser.parse_args()
 
-    with download_asap() as asap_dir:
+    asap_dir = download_asap()
+    try:
         index = make_index(Path(str(asap_dir)))
         if args.modify:
             new_def = modify_maestro_definifion(index)
             json.dump(new_def,
-                      open(Path(THISDIR) / 'definitions' / 'Maestro.json'))
+                      open(Path(THISDIR) / 'definitions' / 'Maestro.json', 'wt'), indent=4)
         else:
             copy_scores(index)
+    finally:
+        shutil.rmtree(Path(str(asap_dir)).parent.parent)
